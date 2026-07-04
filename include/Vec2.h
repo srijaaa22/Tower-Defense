7f67aee2e4c9a3345dbd772a23263347215850be
srija2206 <srijab.2208@gmail.com>
2026-07-04 17:42:18 +0530

    Vec2 struct with operations defined

---
+++ include/Vec2.h
@@ -0,0 +1,28 @@
+#include <cmath>
+
+struct Vec2 {
+    float x;
+    float y;
+    Vec2(): x(0),y(0) {}
+    Vec2(float a, float b) : x(a), y(b) {}
+
+    Vec2 operator+(const Vec2& other) const{
+        return {x + other.x, y + other.y};
+    }
+
+    Vec2 operator*(const float dt) const{
+        return {x*dt, y*dt};
+    }
+
+    float length() const{
+        float len;
+        len = std::sqrt(std::pow(x,2) + std::pow(y,2));
+        return len;
+    }
+
+    Vec2 normalised() const{
+        float div = length();
+        return {x/div, y/div};
+    }
+
+};
