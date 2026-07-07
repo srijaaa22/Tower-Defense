294c07a1c401e76e4e7438d5a694f3e4bfd4252d
Srija Baratam <srijab.2208@gmail.com>
2026-07-07 19:45:19 +0530

    Implemented the Entity Class

---
+++ include/Entity.h
@@ -0,0 +1,13 @@
+#include "Vec2.h"
+#include <SDL2/SDL.h>
+
+class Entity{
+    protected: 
+        Vec2 position;
+        Vec2 size;
+        Entity(Vec2 p, Vec2 s) : position(p), size(s) {} 
+    public:
+        virtual void update(float dt) = 0;
+        virtual void render(SDL_Renderer* renderer) = 0;
+        virtual ~Entity() = default;
+};
